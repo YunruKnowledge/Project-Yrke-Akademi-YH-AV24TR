78f25b8909666ec62ac2a4af6175927e38de346e
Roki <roksandiclazar@gmail.com>
2024-09-26 11:06:35 +0200

    Little changes

--- index.c
+++ index.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-void main(){
+int main(){
     for (int i = 1; i <= 3; i++)
     {
         printf("%d \n", i);
     }
 
-    for (int i = 3; i > 0; i--)
+    for (int i = 2; i > 0; i--)
     {
         printf("%d \n", i);
     }
     
-    return;
+    return 0;
 }
