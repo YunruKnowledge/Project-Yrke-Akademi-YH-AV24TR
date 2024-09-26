ff820d1ae06ebc187f3726f603082ea1a7cb0850
Roki <roksandiclazar@gmail.com>
2024-09-26 10:59:07 +0200

    Loop counter

--- index.c
+++ index.c
@@ -1,41 +1,15 @@
 #include <stdio.h>
 
-#define countof(a) (int)(sizeof(a) / sizeof(*(a)))
-
-#define characters    \
-    X(Smile_emoji, 23333) \
-    X(Y,      15) \
-    X(ENUM25, 25) \
-    X(Z,      31) \
-    X(ENUM40, 40)
-
-#define X(k, v) k = v,
-typedef enum {characters} Characters;
-#undef X
-
-#define X(k, v) {#k, v},
-struct {
-    char  *key;
-    Characters value;
-} Characterss[] = { characters };
-#undef X
-
 void main(){
-    int num;
-
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    if (num == 80085){
-        printf("\033[0;37m (\033[0;31m.\033[0;37m)(\033[0;31m.\033[0;37m) \n");
-        return;
+    for (int i = 1; i <= 3; i++)
+    {
+        printf("%d \n", i);
     }
 
-    for (int i = 0; i < countof(Characterss); i++){
-        if (num == Characterss[i].value){
-            printf("%s \n", Characterss[i].key);
-            return;
-        }
-    } 
-    printf("You are gay \n");
+    for (int i = 3; i > 0; i--)
+    {
+        printf("%d \n", i);
+    }
+    
+    return;
 }
