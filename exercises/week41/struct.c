4f7f7ef3c743bac47b936dbf6e04efae48c6fdc3
Ahmet Sami <ahmetbugatti@gmail.com>
2024-10-09 16:05:38 +0200

    Now struct.c works, apperently i had forgotten to add the slogan char var(slogan) to the printf

--- exercises/week41/struct.c
+++ exercises/week41/struct.c
@@ -1,35 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
-    struct fakeObject {
-        int age;
-        enum Skill { Hook, Punch, Belittle} skill;
-        char name[20];
-        char slogan[87];
-    };
+struct fakeObject {
+    int age;
+    char name[100];
+    char slogan[100];
+};
 
-int main(void){
+int main(void) {
+    
     struct fakeObject nobject;
-    strcpy(nobject.name, "Pyke Fumosson");
+    
+    strcpy(nobject.name, "PykeFumosson");
     nobject.age = 40;
+    
+    // Uncomment this line to assign the slogan
     strcpy(nobject.slogan, "Captains told me to catch the fumonsters, but the fumonsters told me to hunt Captains");
 
-    printf("Your age is: %d,\n Your name is: %s.\nYour favourite line is: %s.", nobject.age, nobject.name);
-}#include <stdio.h>
-#include <string.h>
-
-    struct fakeObject {
-        int age;
-        enum Skill { Hook, Punch, Belittle} skill;
-        char name[20];
-        char slogan[87];
-    };
-
-int main(void){
-    struct fakeObject nobject;
-    strcpy(nobject.name, "Pyke Fumosson");
-    nobject.age = 40;
-    strcpy(nobject.slogan, "Captains told me to catch the fumonsters, but the fumonsters told me to hunt Captains");
-
-    printf("Your age is: %d,\n Your name is: %s.\nYour favourite line is: %s.", nobject.age, nobject.name);
+    // Print the slogan only if it's initialized
+    printf("Your age is: %d,\nYour name is: %s,\nYour favourite line is: %s\n", nobject.age, nobject.name, nobject.slogan);
+    
+    return 0;
 }
+
