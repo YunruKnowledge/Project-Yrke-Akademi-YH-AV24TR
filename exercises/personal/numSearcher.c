41cc3d8634f372124741a5a4e3cf35bfe81acfff
Ahmet Sami <76931121+Yaq8bi@users.noreply.github.com>
2024-10-09 23:06:17 +0200

    NOTES.MD updated with stash commands

--- exercises/personal/numSearcher.c
+++ exercises/personal/numSearcher.c
@@ -1,2 +1,4 @@
 /*create an array of random nums, take input from user and run a for loop,
  where it will run the size of that array and prints if the input value is found in the array.*/
+
+ int var = 88;
