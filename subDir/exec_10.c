7fe4fb1c6474520fd8671dd14413e208fee56b84
Yunru <88000687+YunruKnowledge@users.noreply.github.com>
2024-10-10 11:50:00 +0200

    #10 exec10 question guard

--- subDir/exec_10.c
+++ subDir/exec_10.c
@@ -3,23 +3,31 @@
 int main(void)
 {
     unsigned char binary[8];
-    unsigned int userNumber;
+    unsigned int userNumber = 0;
+    int exit = 0;
 
-    scanf("%i", &userNumber);
-
-    if (userNumber < 0 && userNumber > 255) return -1;
-
-    for (int i = 0; i <= 8; i++)
+    printf("Enter a number to convert into Byte.\n");
+    while (exit == 0)
     {
-        binary[i] = userNumber % 2;
-        userNumber = (userNumber / 2);
-    }
-    
+        scanf("%i", &userNumber);
+        if (userNumber > 255u)
+        {
+            printf("Try again, enter a number below 255.\n");
+            continue;
+        }
 
-    for (int a = sizeof(binary) -1; a >= 0; a--)
-    {
-        printf("%i", binary[a]);
-    }
+        for (int i = 0; i <= 8; i++)
+        {
+            binary[i] = userNumber % 2;
+            userNumber = (userNumber / 2);
+        }
 
+        for (int a = sizeof(binary) - 1; a >= 0; a--)
+        {
+            printf("%i", binary[a]);
+        }
+        printf("\n");
+        exit = 1;
+    }
     return 0;
 }
