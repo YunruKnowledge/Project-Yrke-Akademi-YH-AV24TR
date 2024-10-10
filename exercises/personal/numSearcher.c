2564249e5900baf41c20783df4f8818189f68cd9
Ahmet Sami <76931121+Yaq8bi@users.noreply.github.com>
2024-10-10 07:40:36 +0200

    wrote the numseacher, look up how to get size of an array?

--- exercises/personal/numSearcher.c
+++ exercises/personal/numSearcher.c
@@ -1,4 +1,20 @@
+#include <stdio.h>
+
 /*create an array of random nums, take input from user and run a for loop,
  where it will run the size of that array and prints if the input value is found in the array.*/
 
- int var = 88;
+int main(void){
+ int nums[] = {20, 30, 50, 80, 99};
+ //how to get size of this array? size_of? do i need <string.h>?
+ int n = 0;
+
+printf("Input a num:___");
+scanf("%i", &n);
+
+ for (int i = 0; i<n; i++){
+    if (nums[i] == n){
+        printf("I found the number %i!", nums[i]);
+        return 0;
+    }
+    return 1;
+ }}
