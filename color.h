1f15f01d18450b5d5f6b7b75e7e285c34335ee37
Roki <roksandiclazar@gmail.com>
2024-10-15 13:40:30 +0200

    proto

---
+++ color.h
@@ -0,0 +1,198 @@
+#ifndef ColorLib
+#define ColorLib
+
+enum usableColors {
+  red,
+  white,
+  blue,
+  black,
+  green,
+  yellow,
+  magenta,
+  cyan,
+};
+
+char SetTextColor(enum usableColors color) {
+  if (color == red)
+    return printf("\033[31m");
+
+  if (color == blue)
+    return printf("\x1b[34m");
+
+  if (color == white)
+    return printf("\x1b[37m");
+
+  if (color == black)
+    return printf("\x1b[30m");
+
+  if (color == green)
+    return printf("\x1b[32m");
+
+  if (color == yellow)
+    return printf("\x1b[33m");
+
+  if (color == magenta)
+    return printf("\x1b[35m");
+
+  if (color == cyan)
+    return printf("\x1b[36m");
+}
+
+char SetBoldTextColor(enum usableColors color) {
+  if (color == red)
+    return printf("\e[1;31m");
+
+  if (color == blue)
+    return printf("\e[1;34m");
+
+  if (color == white)
+    return printf("\e[1;37m");
+
+  if (color == black)
+    return printf("\e[1;30m");
+
+  if (color == green)
+    return printf("\e[1;32m");
+
+  if (color == yellow)
+    return printf("\e[1;33m");
+
+  if (color == magenta)
+    return printf("\e[1;35m");
+
+  if (color == cyan)
+    return printf("\e[1;36m");
+}
+
+char SetUnderlineTextColor(enum usableColors color) {
+  if (color == red)
+    return printf("\e[4;31m");
+
+  if (color == blue)
+    return printf("\e[4;34m");
+
+  if (color == white)
+    return printf("\e[4;37m");
+
+  if (color == black)
+    return printf("\e[4;30m");
+
+  if (color == green)
+    return printf("\e[4;32m");
+
+  if (color == yellow)
+    return printf("\e[4;33m");
+
+  if (color == magenta)
+    return printf("\e[4;35m");
+
+  if (color == cyan)
+    return printf("\e[4;36m");
+}
+
+char SetTextHighIntenstyColor(enum usableColors color) {
+  if (color == red)
+    return printf("\e[1;91m");
+
+  if (color == blue)
+    return printf("\e[1;94m");
+
+  if (color == white)
+    return printf("\e[1;97m");
+
+  if (color == black)
+    return printf("\e[1;90m");
+
+  if (color == green)
+    return printf("\e[1;92m");
+
+  if (color == yellow)
+    return printf("\e[1;93m");
+
+  if (color == magenta)
+    return printf("\e[1;95m");
+
+  if (color == cyan)
+    return printf("\e[4;96m");
+}
+
+char SetBoldTextHighIntenstyColor(enum usableColors color) {
+  if (color == red)
+    return printf("\e[1;91m");
+
+  if (color == blue)
+    return printf("\e[1;94m");
+
+  if (color == white)
+    return printf("\e[1;97m");
+
+  if (color == black)
+    return printf("\e[1;90m");
+
+  if (color == green)
+    return printf("\e[1;92m");
+
+  if (color == yellow)
+    return printf("\e[1;93m");
+
+  if (color == magenta)
+    return printf("\e[1;95m");
+
+  if (color == cyan)
+    return printf("\e[1;96m");
+}
+
+char SetBGColor(enum usableColors color) {
+  if (color == red)
+    return printf("\033[41m");
+
+  if (color == blue)
+    return printf("\x1b[44m");
+
+  if (color == white)
+    return printf("\x1b[47m");
+
+  if (color == black)
+    return printf("\x1b[40m");
+
+  if (color == green)
+    return printf("\x1b[42m");
+
+  if (color == yellow)
+    return printf("\x1b[43m");
+
+  if (color == magenta)
+    return printf("\x1b[45m");
+
+  if (color == cyan)
+    return printf("\x1b[46m");
+}
+
+char SetBGHighIntenstyColor(enum usableColors color) {
+  if (color == red)
+    return printf("\e[1;101m");
+
+  if (color == blue)
+    return printf("\e[1;104m");
+
+  if (color == white)
+    return printf("\e[1;107m");
+
+  if (color == black)
+    return printf("\e[1;100m");
+
+  if (color == green)
+    return printf("\e[1;102m");
+
+  if (color == yellow)
+    return printf("\e[1;103m");
+
+  if (color == magenta)
+    return printf("\e[1;105m");
+
+  if (color == cyan)
+    return printf("\e[1;96m");
+}
+
+char ResetColors() { return printf("\x1b[m"); }
+#endif
--- index.c
+++ index.c
@@ -1,36 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "color.h"
 
-#define MAX_HAND 3      // Rock
-#define MIN_HAND 1      // Paper
-#define MIDDLE_HAND 2   // Scissors
+#define MAX_HAND 99
+#define MIN_HAND 0 
 
-int aiHand;
+unsigned int aiNumber, playerNumber, numOfTrys = 10;
 
 void printRandoms(int min, int max, int count) {
     srand(time(0));
 
     for (int i = 0; i < count; i++) {
         int random_number = rand() % (max - min + 1) + min;
-        aiHand = random_number;
+        aiNumber = random_number;
+    }
+}
+
+int Play(){
+    if (numOfTrys != 0){
+        printf("Choose a number between 0 and 99: ");
+        scanf("%i", &playerNumber);
+
+        if (playerNumber < aiNumber){
+            SetTextColor(red);
+            printf("Sorry, too small. Try again! \n");
+            ResetColors();
+            numOfTrys--;
+            PLay();
+        }
+        else if (playerNumber > aiNumber){
+            SetTextColor(red);
+            printf("Sorry, too big. Try again! \n");
+            ResetColors();
+            numOfTrys--;
+            PLay();
+        }
+        else if (playerNumber == aiNumber){
+            SetTextColor(green);
+            printf("Congratulation you guessed the right number \n");
+            ResetColors();
+            isGoingToPlay();
+        }
+
+    }
+    else{
+        SetTextColor(red);
+        printf("Sorry you dont have eny gusses left \n");
+        ResetColors();
+        isGoingToPlay();
+    }
+}
+
+int isGoingToPlay(){
+    int isPlaying;
+    printRandoms(MIN_HAND, MAX_HAND, 1);
+    printf("If you want to play again write 1 or if you want to exit write 2 \n");
+    scanf("%i", &isPlaying);
+    if (isPlaying == 2)
+        return 0;
+    else if (isPlaying == 1){
+        numOfTrys = 10;
+        system("clear");
+        Play();
+    }
+    else{
+        printf("Please enter a  number between 1 or 2");
+        isGoingToPlay();
     }
 }
 
 int main(){
-    int playerHand;
-    printRandoms(MIN_HAND, MAX_HAND, 1);
-    printf("Choose the metod to fight by writing the number before the metod: \n 1. Paper \n 2. Scissors \n 3. Rock \n");
-    scanf("%i", &playerHand);
-
-    printf("You chose: %d \n AI chose: %d \n", playerHand, aiHand);
-
-    if (playerHand == MAX_HAND && aiHand == MIN_HAND || playerHand == MIN_HAND && aiHand == MIDDLE_HAND || playerHand == MIDDLE_HAND && aiHand == MAX_HAND)
-        printf("You Lost! \n");
-    else if (playerHand == aiHand)
-        printf("DRAW! \n");
-    else
-        printf("You WON!!! \n");
-
-    return  0;
+    system("clear");
+    isGoingToPlay();
 }
