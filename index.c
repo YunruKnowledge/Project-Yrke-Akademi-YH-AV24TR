d7a09f0cb74a45334f684850cd6b003edeaf5f39
Roki <roksandiclazar@gmail.com>
2024-10-15 14:08:21 +0200

    bug fixing and optimazing

--- index.c
+++ index.c
@@ -1,76 +1,73 @@
+#include "color.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "color.h"
+#include <stdbool.h>
 
 #define MAX_HAND 99
-#define MIN_HAND 0 
+#define MIN_HAND 0
+#define MAX_AMAOUNT_OF_TRYS 10
 
-unsigned int aiNumber, playerNumber, numOfTrys = 10;
+unsigned int aiNumber, playerNumber, numOfTrys = MAX_AMAOUNT_OF_TRYS;
+bool isPlaying = false;
 
 void printRandoms(int min, int max, int count) {
     srand(time(0));
 
-    for (int i = 0; i < count; i++) {
-        int random_number = rand() % (max - min + 1) + min;
-        aiNumber = random_number;
-    }
+    int random_number = rand() % (max - min + 1) + min;
+    aiNumber = random_number;
 }
 
-int Play(){
-    if (numOfTrys != 0){
-        printf("Choose a number between 0 and 99: ");
-        scanf("%i", &playerNumber);
+int Play() {
+    do {
+    printf("Choose a number between 0 and 99: ");
+    scanf("%i", &playerNumber);
 
-        if (playerNumber < aiNumber){
-            SetTextColor(red);
-            printf("Sorry, too small. Try again! \n");
-            ResetColors();
-            numOfTrys--;
-            PLay();
-        }
-        else if (playerNumber > aiNumber){
-            SetTextColor(red);
-            printf("Sorry, too big. Try again! \n");
-            ResetColors();
-            numOfTrys--;
-            PLay();
-        }
-        else if (playerNumber == aiNumber){
-            SetTextColor(green);
-            printf("Congratulation you guessed the right number \n");
-            ResetColors();
-            isGoingToPlay();
-        }
+    if (playerNumber < aiNumber) {
+      SetBoldTextColor(red);
+      printf("Sorry, too small. Try again! \nNumber of guesses left: %i \n", numOfTrys);
+      ResetColors();
+      numOfTrys--;
+    } else if (playerNumber > aiNumber) {
+      SetBoldTextColor(red);
+      printf("Sorry, too big. Try again! \nNumber of guesses left: %i \n", numOfTrys);
+      ResetColors();
+      numOfTrys--;
+    } else if (playerNumber == aiNumber) {
+      SetBoldTextColor(green);
+      printf("Congratulation you guessed the right number \n");
+      ResetColors();
+      isGoingToPlay();
+    }
+  } while (numOfTrys <= MAX_AMAOUNT_OF_TRYS);
 
-    }
-    else{
-        SetTextColor(red);
-        printf("Sorry you dont have eny gusses left \n");
-        ResetColors();
-        isGoingToPlay();
-    }
+    SetBoldTextColor(red);
+    printf("Sorry you dont have eny gusses left \n");
+    ResetColors();
+    isGoingToPlay();
 }
 
-int isGoingToPlay(){
-    int isPlaying;
-    printRandoms(MIN_HAND, MAX_HAND, 1);
+int isGoingToPlay() {
+  int changeState;
+  printRandoms(MIN_HAND, MAX_HAND, 1);
+  isPlaying = false;
+
+  do{ 
     printf("If you want to play again write 1 or if you want to exit write 2 \n");
-    scanf("%i", &isPlaying);
-    if (isPlaying == 2)
-        return 0;
-    else if (isPlaying == 1){
-        numOfTrys = 10;
+    scanf("%i", &changeState);
+
+    if (changeState == 1){
+        numOfTrys = MAX_AMAOUNT_OF_TRYS;
         system("clear");
         Play();
+        isPlaying = true;
     }
-    else{
-        printf("Please enter a  number between 1 or 2");
-        isGoingToPlay();
-    }
+    else if (changeState == 2)
+        return 0;
+  } while (!isPlaying);
 }
 
-int main(){
-    system("clear");
-    isGoingToPlay();
+int main() {
+  system("clear");
+  isGoingToPlay();
 }
