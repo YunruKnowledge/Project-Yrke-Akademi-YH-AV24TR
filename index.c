7a50b5caf51c1dc7afb43cf3ae42cd22678259d0
Roki <roksandiclazar@gmail.com>
2024-10-15 09:38:12 +0200

    Working Game!

--- index.c
+++ index.c
@@ -1,36 +1,36 @@
-#include <ctype.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_HAND 3      // Rock
+#define MIN_HAND 1      // Paper
+#define MIDDLE_HAND 2   // Scissors
+
+int aiHand;
+
+void printRandoms(int min, int max, int count) {
+    srand(time(0));
+
+    for (int i = 0; i < count; i++) {
+        int random_number = rand() % (max - min + 1) + min;
+        aiHand = random_number;
+    }
+}
 
 int main(){
-    char *name, *aftername;
+    int playerHand;
+    printRandoms(MIN_HAND, MAX_HAND, 1);
+    printf("Choose the metod to fight by writing the number before the metod: \n 1. Paper \n 2. Scissors \n 3. Rock \n");
+    scanf("%i", &playerHand);
 
-    system("clear");
+    printf("You chose: %d \n AI chose: %d \n", playerHand, aiHand);
 
-    printf("Enter your name: ");
-    scanf("%s", &*name);
+    if (playerHand == MAX_HAND && aiHand == MIN_HAND || playerHand == MIN_HAND && aiHand == MIDDLE_HAND || playerHand == MIDDLE_HAND && aiHand == MAX_HAND)
+        printf("You Lost! \n");
+    else if (playerHand == aiHand)
+        printf("DRAW! \n");
+    else
+        printf("You WON!!! \n");
 
-    printf("Enter your name: ");
-    scanf("%s", &*aftername);
-
-    for (int i = 0; i < sizeof(name); i++)
-    {
-        if (name[i] >= 'a' && name[i] <= 'z' && i == 0)
-            name[i] = toupper(name[i]);
-        else if (name[i] >= 'A' && name[i] <= 'Z' && i != 0)
-            name[i] = tolower(name[i]);
-    }
-
-    for (int i = 0; i < sizeof(aftername); i++)
-    {
-        if (aftername[i] >= 'a' && aftername[i] <= 'z' && i == 0)
-            aftername[i] = toupper(aftername[i]);
-        else if (aftername[i] >= 'A' && aftername[i] <= 'Z' && i != 0)
-            aftername[i] = tolower(aftername[i]);
-    }
-
-    system("clear");
-    printf("%s %s \n", name, aftername);
-
-    return 0;
+    return  0;
 }
