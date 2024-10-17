7c70a28e7aacc7ecd52efa1cd9c405149cc7b35c
Roki <roksandiclazar@gmail.com>
2024-10-17 13:25:31 +0200

    New lib && lib fix!

---
+++ Librarys/GRN.c
@@ -0,0 +1,20 @@
+#include "GRN.h"
+
+int getRandomNumber(int min, int max) {
+    unsigned int seed = time(0);
+    int rd_num = rand_r(&seed) % (max - min + 1) + min;
+    return rd_num;
+}
+
+int* getArrayOfRandomNumbers(int min, int max, int count) {
+    if (count <= 0 || min >= max) return NULL;
+
+    int* numbers = (int*)malloc(count * sizeof(int));
+    if (!numbers) return NULL;
+
+    srand(time(NULL));
+    for (int i = 0; i < count; i++) {
+        numbers[i] = min + rand() % (max - min);
+    }
+    return numbers;
+}
---
+++ Librarys/GRN.h
@@ -0,0 +1,9 @@
+#ifndef GENERATE_RANDOM_NUMBER_NUM
+#define GENERATE_RANDOM_NUMBER_NUM
+
+int getRandomNumber(int min, int max);
+
+int* getArrayOfRandomNumbers(int min, int max, int count);
+
+#include "GRN.c"
+#endif // !GENERATE_RANDOM_NUMBER_NUM
