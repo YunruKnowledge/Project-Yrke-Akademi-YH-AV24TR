c5a9c35daf1528184b9c2f66a2ba6646c61c617b
Roki <roksandiclazar@gmail.com>
2024-10-17 14:53:01 +0200

    Drip

--- index.c
+++ index.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "Librarys/GRN.h"
+#include "Librarys/colors.h"
 
 #define MIN_NUMBER 0
 #define MAX_NUMBER 10000
@@ -12,20 +13,29 @@ int compare(const void* a, const void* b){
 int main(){
     int* numbers = getArrayOfRandomNumbers(MIN_NUMBER, MAX_NUMBER, NUMBER_OF_RANDOM_NUMBERS);
     int num[NUMBER_OF_RANDOM_NUMBERS + 1];
+
+    printf("Unsorted Array: { ");
     for(int i = 0; i <= NUMBER_OF_RANDOM_NUMBERS; i++){
-        printf("%i ",numbers[i]);
+        SetBoldTextColor(red);
+        SetBGHighIntenstyColor(white);
+        printf("%i",numbers[i]);
+        if (i < NUMBER_OF_RANDOM_NUMBERS) printf(", ");
         num[i] = numbers[i];
     }
+    ResetColors();
 
     int n = sizeof(num) / sizeof(num[0]);
     qsort(num, n, sizeof(int), compare);
-    printf("\nSorted Array: \n");
 
+    printf(" } \n  Sorted Array: { " );
     for (int i = 0; i < n; i++) {
-        printf("%d ", num[i]);
+        SetBoldTextColor(white);
+        SetBGHighIntenstyColor(red);
+        printf("%d", num[i]);
+        if (i < n - 1) printf(", ");
     }
-
-    printf("\n");
+    ResetColors();
+    printf(" }\n");
 
     return 0;
 }
