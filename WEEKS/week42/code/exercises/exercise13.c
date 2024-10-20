7704837f486c73d55633f305e0c9bb0bbedff8d4
Ahmet Sami <76931121+Yaq8bi@users.noreply.github.com>
2024-10-20 23:37:37 +0200

    Week 42 done. Everythings finished Will switch to Week 43.

--- WEEKS/week42/code/exercises/exercise13.c
+++ WEEKS/week42/code/exercises/exercise13.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-#include <ctype.h>
+// #include <ctype.h>
 
 int main(void){
 
 int theNum = 0;
+printf("Check for seven in the range of 0 and x, what is x?: ");
 scanf(" %i", &theNum);
 
-if (isdigit(theNum)){
-    printf(" %i", theNum);
-}
-else{
-    printf("Wrong!");
+for (int i = 0; i<=theNum; i++){
+    if (i % 10 == 7){
+        printf("%i\n", i);
+    }
 }
 
 return 0;
 }
+// if (isdigit(theNum)){
+//     printf(" %i", theNum);
+// }
+// else{
+//     printf("Wrong!");
+// }
---
+++ WEEKS/week42/code/exercises/exercise14.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isAPrimeNumber(int num);
+
+int main(void){
+    startAgain:
+    int theNum = 0;
+    printf("Input a number between 2 and 10k:\n");
+    scanf("%i", &theNum);
+
+    if (theNum <= 1){
+        printf("I said above 2!\n");
+        goto startAgain;
+    }else if(theNum >= 11000){
+        printf("I said max 10k!\n");
+        goto startAgain;
+    }else{
+        if (isAPrimeNumber(theNum)){
+            printf("The number %d is a prime number!", theNum);
+        }else{
+            printf("The number %d was NOT a prime numer.", theNum);
+        }
+    }
+    return 0;
+}
+
+bool isAPrimeNumber(int num){
+    //check for numbers that are less than 2
+    if (num <= 2){
+        return false;
+    }
+    //check divisability from 2 upto num-1
+    for (int i = 2; i < num; i++){
+        if (num % i == 0){
+            return false;// if divisible by any number other than 1 and itself
+        }
+    }
+    return true;
+}
