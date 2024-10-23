54276b605bbc2e223fcf1c9a76177b5a415879cd
Ahmet Sami <76931121+Yaq8bi@users.noreply.github.com>
2024-10-23 23:19:59 +0200

    Pointer Manipulation done.

--- WEEKS/week43/code/pointerTest.c
+++ WEEKS/week43/code/pointerTest.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-char name[50] = "noWay";
-char *ptrToName = name;
+char name[20] = "Go Home";
+char *p = name;
 int main(void){
 //Faroch example below
 /*
-    const char *name1 = "Okhmet";//pointer
     char myName[8] = "Ehmet";//array with Name
-    char *firstLetterOfName1 = (char *)name1;// point to the first char of Akhmet
+    const char *name1 = "Okhmet";//pointer, out of the Okhmet that it points to, due to the variables type, it can only hold the O of this variable.
+    char *firstLetterOfName1 = (char *)name1;//point to the first char of Akhmet
     char **pp = &firstLetterOfName1;
 
     *pp = myName;
@@ -17,7 +17,14 @@ int main(void){
     printf("%s\n", firstLetterOfName1);
 */
 //different test below
-    char **ptr = &ptrToName;//double asterisk,bcz im pointing to a pointer
-    printf("ptr: %c\n", **ptr);
+    char **pp = &p;//double asterisk,bcz im pointing to a pointer
+    char ***ppp = &pp;
+    *(**ppp+2) = 'j';
+    *(**ppp+3) = 'o';
+    *(**ppp+4) = ' ';
+    *(**ppp+5) = ' ';
+    *(**ppp+6) = 'O';
+    *(**ppp+7) = 'P';
 
+    printf("ptrAddress: %c\nThe manipulatedName: %s\n", ***ppp, name);//The value that the pointer is pointing to.
 }
