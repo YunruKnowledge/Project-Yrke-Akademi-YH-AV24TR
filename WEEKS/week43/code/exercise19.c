ac6dbb0ebbbcb2466fc2ace2dff7200f54f0e91d
Ahmet Sami <76931121+Yaq8bi@users.noreply.github.com>
2024-10-27 00:55:26 +0200

    Exercise19 done using getchar and while loop.

--- WEEKS/week43/code/exercise19.c
+++ WEEKS/week43/code/exercise19.c
@@ -1,13 +1,38 @@
+// int main(void){
+//     //a for loop to loop through all the chars of an array.
+//     //if space printf \n
+
 #include <stdio.h>
+// #include <string.h>
 
-int main(void){
-    //a for loop to loop through all the chars of an array.
-    //if space printf \n
+int main(){
+   
+   int ch;
+   printf("Enter a string now!: ");
 
-    char name[20];
-    printf("Input a name: ");
-    scanf(" %s", &name);
+   while(( ch = getchar() ) != '\n'){
+    if(ch == ' '){
+        putchar('\n');
+    }else {
+        putchar(ch);
+    }
+   } 
+   putchar('\n');
+   
+   /* char input[100];//buffer to store the string
+    char *token; //pointer to hold the parts of strings
 
-    printf(" %s", name);
+    printf("Enter a string boi!: ");
+    fgets(input, sizeof(input),stdin);//read the input with the spaces: size byte, max size, string with spaces.
 
+    input[strcspn(input, "/n")] = 0;//remove the newline char if its present at the end.
+
+    token = strtok(input, " ");//find spaces, print the string that is before the space
+
+    while (token !=NULL){
+        printf("%s\n", token);//print one part of the strings
+        token = strtok(NULL, " ");// get the next token
+    }
+    */
+    return 0;
 }
