9edc5f498f849f8c3e46b3becbf36ae4cc269635
Ahmet Sami <76931121+Yaq8bi@users.noreply.github.com>
2024-11-13 11:15:52 +0100

    wordFinder from file finished.

---
+++ WEEKS/week46/wedWordFinder.c
@@ -0,0 +1,49 @@
+// Write a program that searches for a specific word in a file named " text.txt".
+//  The program should ask the user for the word to search for,
+//  and then print all lines in the file that contain this word.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER 50
+#define FILE_NAME "text.txt"
+
+int main(void)
+{
+    FILE *file; /*=  fopen("text.txt", "r")*/
+
+    char theWord[BUFFER];
+    char temp[BUFFER];
+
+    file = fopen(FILE_NAME, "r");
+    if (file == NULL)
+    {
+        perror("Couldn't open the file");
+        exit(1);
+    }
+    else
+    {
+        printf("FILE EXISTS!\n");
+    }
+    printf("Input a word to search for:\n");
+    scanf(" %s", theWord);
+
+    int count = 0;
+
+    // put to temp, read buffer amount, the file
+    while (fgets(temp, BUFFER, file) != NULL)
+    {
+        if (strstr(temp, theWord) != NULL)
+        {
+            printf("Found your word: %s\nTHE LINE BELOW:\n%s\n", theWord, temp);
+            count++;
+        }
+
+        // printf("Lines containing the word '%s':\n", theWord);
+    }
+
+        printf("There is ~~%i~~ lines in the file that had your word.", count);
+    fclose(file); // close the file
+
+    return 0;
+}
