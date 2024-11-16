4512abb1b749ecaae115085d51ad51ef97252783
Ahmet Sami <76931121+Yaq8bi@users.noreply.github.com>
2024-11-16 14:53:55 +0100

    The Luhn Algorithm personnummer check finished.

--- WEEKS/week46/Assignments/luhnAlgTest.c
+++ WEEKS/week46/Assignments/luhnAlgTest.c
@@ -11,25 +11,11 @@ bool validateNum(char *perNum);
 
 int main()
 {
-    /*
-    char *userInput = "0201034915";
+    char userInput[BUFFER];
     printf("Input a personnummer:\n");
-    scanf(" %s", &userInput);
+    scanf(" %s", userInput);
 
     printf("Personnummer is: %s\nAnd its a %s", userInput, validateNum(userInput) ? "Valid number" : "Invalid number!");
-
-    return 0;
-*/
-    char userInput[BUFFER]; // buffer to hold personnummer 10 number and if dash.
-    printf("Enter personnummer (YYMMDDNNNN or YYMMDD-NNNN): ");
-    scanf("%11s", userInput);
-
-    if (validateNum(userInput)) // validate returns true
-        printf("Personnummer is: Valid number!\n");
-    else // else return false
-    {
-        printf("Personnummer is: Invalid number!\n");
-    }
 
     return 0;
 }
@@ -38,52 +24,102 @@ int main()
 bool validateNum(char *perNum)
 {
     int length = 0; // Length of the personnummer.
-    while (perNum[length] != '\0')
-    {
-        // To find the length of the string/input, add 1 to the length until we find the Null char that ends or show that the string is ended.
-        length++;
-    }
+    // while (perNum[length] != '\0')
+    // {
+    //     // To find the length of the string/input, add 1 to the length until we find the Null char that ends or show that the string is ended.
+    //     length++;
+    // }
 
-    int numHolder = 0;
-    // total.
+    // int numHolder = 0;
+    // total after everything is done.
     int total = 0;
-    // Luhn Algorithm needs this. To know if we multiply the number or avoid.
-    int oddOrEven = length % 2;
+    // Luhn Algorithm needs this. for determining position parity.
+    int oddOrEven = 0;
+    // checking every char, to find the length, only the digits will add 1 to length.
 
-    // a loop to start from right digit, and end at the leftmost number, which most likely is a 1 or 2.
-    for (int i = length - 1; i >= 0; i--)
+    for (int i = 0; perNum[i] != '\0'; i++)
     {
-
-        // check if the num we look at is a number with a library.
-        if (isdigit(perNum[i]) == false)
+        if (isdigit(perNum[i]))//check every number, if its a digit, +1, OR if its not a digit AND not a dash(-) return false/wrong format.
         {
+            length++;
+        }
+        else if (perNum[i] != '-')
+        {
+            printf("\nWrong format!\n");
             return false;
         }
-        else if (perNum[i] == '-')
+        
+    }
+
+    if (length != 10)
+    {
+        return false;
+    }
+    
+
+    oddOrEven = length % 2;
+    int position = 0; // numbers position indicater.
+    for (int i = 0; perNum[i] != '\0'; i++)
+    {
+        if (!isdigit(perNum[i]))
         {
-            continue;
+            continue; // perNum[i] will go to the next step/i++. skiping this iteration. Because the perNum[i] is pointing/at position where it is not a number, basically a dash.
         }
+        // else  { }
+        int numHolder = perNum[i] - '0'; // hold the number, and turn from char into an integer.
 
-        // converts the number from string to a INTEGER!
-        numHolder = perNum[i] - '0';
-
-        // if the i mod 2 is equal to oddOrEven, *= numHolder by 2.
-        // and if numHolder is more than 9, -= oddOrEven by 9.
-        if (i % 2 == oddOrEven)
+        // Because of luhn algorithm, we haveto multiply every 2 number. So we multiply by 2. 
+        if (position % 2 == oddOrEven)
         {
             numHolder *= 2;
-
-            if (numHolder > 9)
+            if (numHolder > 9) // If the number ismore than 9, - 9 the number to its self.
             {
                 numHolder -= 9;
             }
         }
         total += numHolder;
+        position++;
     }
 
-    // if (total % 10 == 0)
-    // {
-    //     return true;
-    // }
+    //if the the total modulus to 10 returns 0 everything is correct. The number is valid.
     return total % 10 == 0;
+    /*
+        // a loop to start from right digit, and end at the leftmost number, which most likely is a 1 or 2.
+        for (int i = length - 1; i >= 0; i--)
+        {
+
+            // check if the num we look at is a number with a library.
+            if (isdigit(perNum[i]) == false)
+            {
+                return false;
+            }
+            else if (perNum[i] == '-')
+            {
+                continue;
+            }
+
+            // converts the number from string to a INTEGER!
+            numHolder = perNum[i] - '0';
+
+            // if the i mod 2 is equal to oddOrEven, *= numHolder by 2.
+            // and if numHolder is more than 9, -= oddOrEven by 9.
+            if (i % 2 == oddOrEven)
+            {
+                numHolder *= 2;
+
+                if (numHolder > 9)
+                {
+                    numHolder -= 9;
+                }
+            }
+            total += numHolder;
+        }
+
+        // if (total % 10 == 0)
+        // {
+        //     return true;
+        // }
+        return total % 10 == 0;
+
+        */
 }
