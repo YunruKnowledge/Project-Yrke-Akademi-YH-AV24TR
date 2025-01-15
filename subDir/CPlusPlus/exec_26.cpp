066ed226b4881fe229e8ebb91ade9dec0bfb9032
Yunru <88000687+YunruKnowledge@users.noreply.github.com>
2025-01-15 11:25:56 +0100

    #10 26 rework

--- subDir/CPlusPlus/exec_26.cpp
+++ subDir/CPlusPlus/exec_26.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 
-template <typename Type> void print(const Type &_val) {
-  std::cout << _val << std::endl;
+template <typename Type> void print(const Type *_array, const size_t _size) {
+  for (size_t i = 0; i < _size; i++) {
+    std::cout << _array[i] << ">";
+  };
+  std::cout << std::endl;
 }
 
-template <typename Type> void print(const Type *_ptr, const size_t _size) {
-  for (size_t i = 0; i < _size; i++) {
-    std::cout << _ptr[i] << std::endl;
-  }
+template <typename Type, typename FunctionType>
+void ptrPrint(const Type *_array, const size_t _size, FunctionType _function) {
+  _function(_array, _size);
 }
 
 int main(void) {
-  print(22);
-  print(2.2f);
-  print(22.2222222);
-
   int array[3]{1, 2, 3};
-  print(array, 3);
+  ptrPrint(array, 3, print<int>);
 
   return 0;
 }
