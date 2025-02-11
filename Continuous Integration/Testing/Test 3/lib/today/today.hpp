47b8b9e8053da703d8afcc883da6c23e92e0ab5f
Lazar <roksandiclazar@gmail.com>
2025-02-11 12:16:08 +0100

    Test 3 dummy lib date successfully implemented!

--- Continuous Integration/Testing/Test 3/lib/today/today.hpp
+++ Continuous Integration/Testing/Test 3/lib/today/today.hpp
@@ -1,36 +1,38 @@
 #ifndef TODAY_HPP
 #define TODAY_HPP
 
-#include "date.hpp"
+#include "IDate.hpp"
 
-class today
-{
-    date date_t;
-
+class today {
     int year;
     int month;
     int day;
 
 public:
-    today(date &_date) : date_t{_date}, year{date_t.getYear()}, month{date_t.getMonth()}, day{date_t.getDay()} {}
+    today(IDate &_date) : year{_date.getYear()}, month{_date.getMonth()}, day{_date.getDay()} {
+    }
 
-    bool isLeapYear(void)
-    {
+    bool isLeapYear(void) {
         bool status{false};
         int days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-        if ((day > days[month - 1]) && (month == 2))
-        {
-            if ((year % 400 == 0) && (year % 4 == 0) && (year % 100 != 0))
-            {
-                if (day == days[month - 1] + 1)
-                {
+        if ((day > days[month - 1]) && (month == 2)) {
+            if ((year % 100 != 0) && (year % 4 == 0) || (year % 400 == 0)) {
+                if (day == days[month - 1] + 1) {
                     status = true;
                 }
             }
         }
 
         return status;
+    }
+
+    std::string showDate(void) {
+        std::string res;
+        res = (std::to_string(day) + std::string("/") + std::to_string(month) + std::string("-") +
+               std::to_string(year));
+
+        return res;
     }
 };
 
--- Continuous Integration/Testing/Test 3/test/test.cpp
+++ Continuous Integration/Testing/Test 3/test/test.cpp
@@ -1,11 +1,31 @@
 #include <gtest/gtest.h>
-#include "date.hpp"
 #include "today.hpp"
+#include "IDate.hpp"
+
+class date : public IDate {
+private:
+    int year;
+    int month;
+    int day;
+
+public:
+    bool setDate(const std::string &date) {
+        int res = std::sscanf(date.c_str(), "%d/%d-%d", &day, &month, &year);
+        return res == 3;
+    }
+
+    int getYear(void) override { return year; };
+
+    int getMonth(void) override { return month; };
+
+    int getDay(void) override { return day; };
+};
 
 TEST(Test, _test) {
     date date;
+    date.setDate("29/2-2024");
     today today{date};
 
-    std::cout << date.getDay() << "/" << date.getMonth() << "-" << date.getYear() << std::endl;
     std::cout << (today.isLeapYear() ? "Today is a leap year!" : "Today is not a leap year") << std::endl;
+    std::cout << today.showDate() << std::endl;
 }
