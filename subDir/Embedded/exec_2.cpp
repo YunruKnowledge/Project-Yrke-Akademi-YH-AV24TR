9877a2a66deab9a584cc36fea1c63f875401100f
SkyThe <88000687+YunruKnowledge@users.noreply.github.com>
2025-03-11 14:35:07 +0100

    #10 exec2 small edit

--- subDir/Embedded/exec_2.cpp
+++ subDir/Embedded/exec_2.cpp
@@ -7,40 +7,46 @@ static int var{1};
 
 void fthread1(void) {
   while (true) {
+    mtx.lock();
     if (var == 1) {
       std::cout << "1 -";
-      mtx.lock();
       var++;
       mtx.unlock();
       break;
+    } else {
+      mtx.unlock();
     }
   }
 }
 void fthread2(void) {
   while (true) {
+    mtx.lock();
     if (var == 2) {
       std::cout << " 2 -";
-      mtx.lock();
       var++;
       mtx.unlock();
       break;
+    } else {
+      mtx.unlock();
     }
   }
 }
 void fthread3(void) {
   while (true) {
+    mtx.lock();
     if (var == 3) {
       std::cout << " 3\n";
-      mtx.lock();
       var = 1;
       mtx.unlock();
       break;
+    } else {
+      mtx.unlock();
     }
   }
 }
 
 int main(void) {
-  for (size_t i = 0; i < 10; i++) {
+  for (size_t i = 0; i < 100; i++) {
     std::thread t1{fthread1};
     std::thread t3{fthread3};
     std::thread t2{fthread2};
@@ -49,7 +55,7 @@ int main(void) {
     t1.detach();
     t3.detach();
     
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
   }
 
   return 0;
