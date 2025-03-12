4a8e3cf9789f4fce9e3396b0dc36ae9240764f38
SkyThe <88000687+YunruKnowledge@users.noreply.github.com>
2025-03-12 12:44:11 +0100

    #10 exec2

--- subDir/Embedded/exec_2.cpp
+++ subDir/Embedded/exec_2.cpp
@@ -1,62 +1,58 @@
+#include <condition_variable>
 #include <iostream>
 #include <mutex>
 #include <thread>
 
 std::mutex mtx;
+static std::condition_variable condition;
 static int var{1};
 
 void fthread1(void) {
   while (true) {
-    mtx.lock();
-    if (var == 1) {
-      std::cout << "1 -";
-      var++;
-      mtx.unlock();
-      break;
-    } else {
-      mtx.unlock();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::unique_lock<std::mutex> lock{mtx};
+    while (var != 1) {
+      condition.wait(lock);
     }
+    std::cout << "1 -";
+    var++;
+    lock.unlock();
+    condition.notify_all();
   }
 }
 void fthread2(void) {
   while (true) {
-    mtx.lock();
-    if (var == 2) {
-      std::cout << " 2 -";
-      var++;
-      mtx.unlock();
-      break;
-    } else {
-      mtx.unlock();
+    std::unique_lock<std::mutex> lock{mtx};
+    while (var != 2) {
+      condition.wait(lock);
     }
+    std::cout << " 2 -";
+    var++;
+    lock.unlock();
+    condition.notify_all();
   }
 }
 void fthread3(void) {
   while (true) {
-    mtx.lock();
-    if (var == 3) {
-      std::cout << " 3\n";
-      var = 1;
-      mtx.unlock();
-      break;
-    } else {
-      mtx.unlock();
+    std::unique_lock<std::mutex> lock{mtx};
+    while (var != 3) {
+      condition.wait(lock);
     }
+    std::cout << " 3\n";
+    var = 1;
+    lock.unlock();
+    condition.notify_all();
   }
 }
 
 int main(void) {
-  for (size_t i = 0; i < 100; i++) {
-    std::thread t1{fthread1};
-    std::thread t3{fthread3};
-    std::thread t2{fthread2};
+  std::thread t1{fthread1};
+  std::thread t3{fthread3};
+  std::thread t2{fthread2};
 
-    t2.detach();
-    t1.detach();
-    t3.detach();
-    
-    // std::this_thread::sleep_for(std::chrono::seconds(1));
-  }
-
+  t2.join();
+  t1.join();
+  t3.join();
+  
   return 0;
 }
